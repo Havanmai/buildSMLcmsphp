cea22fd67bad9c5e5e1015fefcc4eea5abcc0352
Havanmai <71115868+Havanmai@users.noreply.github.com>
2022-05-13 15:04:54 +0700

    addmenu

--- cms.blade.php
+++ cms.blade.php
@@ -18,7 +18,7 @@
 
 <body>
   <cms-root></cms-root>
-<script src="/cms/runtime.5ab13fd89c84bdca3920.js" defer></script><script src="/cms/polyfills.4824a1f71545661e2585.js" defer></script><script src="/cms/main.e73b4749115847fa3429.js" defer></script>
+<script src="/cms/runtime.dd64b7cdb2b22e3358a2.js" defer></script><script src="/cms/polyfills.4824a1f71545661e2585.js" defer></script><script src="/cms/main.f2206bca92427c40df79.js" defer></script>
 
 
 </body></html>

c23ef1062178d55a4583f434af8ccc821444d564
Havanmai <71115868+Havanmai@users.noreply.github.com>
2022-05-14 09:36:06 +0700

    user and permission

--- cms.blade.php
+++ cms.blade.php
@@ -18,7 +18,7 @@
 
 <body>
   <cms-root></cms-root>
-<script src="/cms/runtime.dd64b7cdb2b22e3358a2.js" defer></script><script src="/cms/polyfills.4824a1f71545661e2585.js" defer></script><script src="/cms/main.f2206bca92427c40df79.js" defer></script>
+<script src="/cms/runtime.2f1da7522a48ae61fca2.js" defer></script><script src="/cms/polyfills.4824a1f71545661e2585.js" defer></script><script src="/cms/main.d9f898541a3765e67362.js" defer></script>
 
 
 </body></html>
